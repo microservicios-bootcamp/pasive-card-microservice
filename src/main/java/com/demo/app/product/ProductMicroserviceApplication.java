0a7e41cc085dcf17f8f967efbbd40a22904f6b8c
Dennilson190414 <103059691+Dennilson190414@users.noreply.github.com>
2022-05-03 11:04:08 -0500

    test git hub action

--- src/main/java/com/demo/app/product/ProductMicroserviceApplication.java
+++ src/main/java/com/demo/app/product/ProductMicroserviceApplication.java
@@ -16,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ProductMicroserviceApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(ProductMicroserviceApplication.class, args);
     }
 
